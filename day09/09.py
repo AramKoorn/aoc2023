e4eb6d7cb8125e9a19796740aa4515eaba49db5c
Aram Koorn <aram.koorn@icloud.com>
2023-12-11 14:07:17 +0100

    day9

---
+++ day09/09.py
@@ -0,0 +1,31 @@
+import re
+
+
+f = open("input.txt", "r")
+lines = f.readlines()
+lines = [x.strip('\n') for x in lines]
+lines = [list(map(int, re.findall(r'-?\d+', x))) for x in lines]
+
+
+def diffs(arr):
+    d = []
+    for i in range(1, len(arr)):
+        d.append(arr[i] - arr[i - 1])
+    return d
+
+t = 0
+for l in lines:
+    last = []
+    diff = diffs(l)
+    last.append(diff[-1])
+    while not all(x == 0 for x in diff):
+        diff = diffs(diff)
+        last.append(diff[-1])
+    debug = True
+    t += sum(last) + l[-1]
+
+print(t)
+
+
+
+
